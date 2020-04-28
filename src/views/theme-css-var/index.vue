1e4957245aa93742ad3411f5b80548a091d24c81
Lily <lili_sakura@126.com>
2020-04-28 16:24:56 +0800

    add file

---
+++ src/views/theme-css-var/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="css-container">
+    <p>Vue + CSS变量 动态换肤</p>
+    <button @click="() => changeTheme('light')">浅色主题</button>
+    <button @click="() => changeTheme('dark')">深色主题</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScssTheme',
+  methods: {
+    changeTheme(type) {
+      document.documentElement.setAttribute("data-theme", type)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+@import "./styles/theme.css";
+
+.css-container {
+  font-size: 18px;
+  color: var(--main-color);
+  background-color: var(--main-background);
+}
+</style>
---
+++ src/views/theme-scss/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="scss-container">
+    <p>Vue + Scss 动态换肤</p>
+    <button @click="() => changeTheme('light')">浅色主题</button>
+    <button @click="() => changeTheme('dark')">深色主题</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScssTheme',
+  methods: {
+    changeTheme(type) {
+      document.documentElement.setAttribute("data-theme", type)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/_handle.scss";
+
+.scss-container {
+  font-size: 18px;
+  @include font_color("main_color");
+  @include background_color("main_background")
+}
+</style>
